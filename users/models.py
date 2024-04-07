7ac6476bf83e946300be9e44f5c120687f15e5d6
KarlaJulyana <karla@solarz.com.br>
2024-04-07 17:04:39 -0300

    Revert "chore: create users model"
    This reverts commit 1f6ad6904f38a40200567732b2d07f8e72cb3f29.

--- users/models.py
+++ users/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-class User(models.Model):
-    name = models.CharField(max_length=30, verbose_name="User name")
-    social_security_card = models.CharField(max_length=11, verbose_name="Social security card")
-
-    def __str__(self):
-        return f"User: {self.name}; Social security card: {self.social_security_card}"
-    
-    class Meta:
-        verbose_name = "User"
-        verbose_name_plural = "Users"
+# Create your models here.
