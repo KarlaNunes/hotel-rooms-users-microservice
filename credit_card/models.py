1811b642c99ede8d84bd220ba1955e2a6a74d774
KarlaJulyana <karla@solarz.com.br>
2024-04-10 16:53:36 -0300

    chore: remove user_id and move foreign key to owner

--- credit_card/models.py
+++ credit_card/models.py
@@ -4,10 +4,8 @@ from users.models import User
 class CreditCard(models.Model):
     number = models.CharField(max_length=16, verbose_name="Card number")
     due_date = models.DateField(verbose_name="Card due date")
-    owner = models.CharField(max_length=30, verbose_name="Credit card owner")
-    cvv = models.CharField(max_length=3, verbose_name="CVV")
-    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    cvv = models.CharField(max_length=3, verbose_name="CVV")    
     
     def __str__(self):
         return f"Credit card: {self.number}; "
